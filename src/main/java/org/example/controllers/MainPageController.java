9b36feff91d2a2b414f1537261b00b2f47f10e75
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-05-22 01:07:57 +0300

    feature: создать контроллеры

---
+++ src/main/java/org/example/controllers/MainPageController.java
@@ -0,0 +1,12 @@
+package org.example.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainPageController {
+    @GetMapping("/mainPage")
+    public String mainPage(){
+        return "mainPage/frame";
+    }
+}
