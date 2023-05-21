80769029737d456406df8c58c6086668ab30d353
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-05-21 23:10:30 +0300

    comment: добавить правку в описание типа

--- src/main/java/org/example/models/PaymentType.java
+++ src/main/java/org/example/models/PaymentType.java
@@ -3,7 +3,7 @@ package org.example.models;
 public enum PaymentType {
     FINES("Штрафы и налоги"),
     STATE_DUTY("Государственная пошлина"),
-    SERVICES("Государственные услуги");
+    SERVICES("Государственные услуги");//услуги коммерция организации/  услуги контрагентов
 
     private final String name;
 
