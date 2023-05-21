942dfd62ec9656bfbc58a97e3b788dca76210601
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-05-21 22:40:57 +0300

    feature: создать первый тестовый контроллер

---
+++ src/main/java/org/example/controllers/GenerateController.java
@@ -0,0 +1,23 @@
+package org.example.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GenerateController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting/greeting";
+    }
+
+//    @GetMapping("/getChart")
+//    public String getChart(@RequestParam()){
+//        return "";
+//    }
+
+
+}
