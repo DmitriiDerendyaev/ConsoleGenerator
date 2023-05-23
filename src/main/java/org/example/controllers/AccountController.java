a023961473d231cdeaf9873f7d6bdd33dc52a227
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-05-24 01:44:26 +0300

    testing: тестировать контроллеры и выводить в консоль

--- src/main/java/org/example/controllers/AccountController.java
+++ src/main/java/org/example/controllers/AccountController.java
@@ -1,12 +1,27 @@
 package org.example.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AccountController {
-    @GetMapping("/account")
+    @GetMapping("/account2")
     public String account(){
+        return "personalAccount/frame";
+    }
+
+    @GetMapping("/account")
+    public String example(Model model) {
+        // Заполняем переменные модели значениями
+        model.addAttribute("variableName", "Вася");
+        model.addAttribute("variableSurname", "Иванов");
+        model.addAttribute("variableRole", "Младший сотрудник");
+        model.addAttribute("variablePatronymic", "Александрович");
+        model.addAttribute("variableEmail", "dds_stalker@mail.ru");
+        model.addAttribute("variablePhoneNumber", "+798888888888");
+
+        // Возвращаем имя шаблона Thymeleaf
         return "personalAccount/frame";
     }
 }
