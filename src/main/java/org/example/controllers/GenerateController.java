07717f450987c90d5c511cd65b90d9e8b68a79f4
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-05-24 18:44:49 +0300

    test: поработать с файлов

--- src/main/java/org/example/controllers/GenerateController.java
+++ src/main/java/org/example/controllers/GenerateController.java
@@ -1,23 +1,44 @@
 package org.example.controllers;
 
+import org.example.models.PaymentParameter;
+import org.example.models.PaymentRegistry;
+import org.example.models.PaymentType;
+import org.example.utils.ChartGenerator;
+import org.example.utils.ParserData;
+import org.example.utils.PrepareData;
+import org.example.utils.PrepareMapToChart;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Controller
 public class GenerateController {
-
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting/greeting";
-    }
 
     @GetMapping("/generate")
     public String generate(){
         return "generationSettings/frame";
     }
+    @PostMapping("/handleButtonClick")
+    public String handleButtonClick(@RequestParam("file") MultipartFile file) throws IOException {
+        // Получение пути к файлу
+        String filePath = file.getOriginalFilename();
+        System.out.println("Путь к файлу: " + filePath);
 
+        PaymentRegistry paymentRegistry = new PaymentRegistry();
+        ParserData parserData = new ParserData();
+        PrepareData prepareData = new PrepareData();
 
+        paymentRegistry = parserData.ReadRegistry(filePath, paymentRegistry);
+
+        ChartGenerator.chartGenerateJSON(PrepareMapToChart.getChartMap(paymentRegistry, PaymentParameter.BPA_NOTICE));
+
+        System.out.println(prepareData.getPaymentAmount(paymentRegistry, PaymentType.FINES.getName()));
+
+        return "generationSettings/frame";
+    }
 }
