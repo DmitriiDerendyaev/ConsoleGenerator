6a2bde92d506cab70fe637069ee6d725e9839303
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-05-25 13:01:39 +0300

    feature: добавлены функции контроллера для обработки запроса

--- src/main/java/org/example/controllers/GenerateController.java
+++ src/main/java/org/example/controllers/GenerateController.java
@@ -7,14 +7,18 @@ import org.example.utils.ChartGenerator;
 import org.example.utils.ParserData;
 import org.example.utils.PrepareData;
 import org.example.utils.PrepareMapToChart;
+import org.jfree.chart.title.Title;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+
 
 @Controller
 public class GenerateController {
@@ -24,21 +28,55 @@ public class GenerateController {
         return "generationSettings/frame";
     }
     @PostMapping("/handleButtonClick")
-    public String handleButtonClick(@RequestParam("file") MultipartFile file) throws IOException {
-        // Получение пути к файлу
-        String filePath = file.getOriginalFilename();
-        System.out.println("Путь к файлу: " + filePath);
+    public String handleButtonClick(@RequestParam("chartType") String chartType,
+                                    @RequestParam("radio") String radio,
+                                    @RequestParam("file") MultipartFile file, Model model) throws IOException {
 
         PaymentRegistry paymentRegistry = new PaymentRegistry();
         ParserData parserData = new ParserData();
-        PrepareData prepareData = new PrepareData();
 
-        paymentRegistry = parserData.ReadRegistry(filePath, paymentRegistry);
+        paymentRegistry = parserData.ReadRegistry(file, paymentRegistry);
 
-        ChartGenerator.chartGenerateJSON(PrepareMapToChart.getChartMap(paymentRegistry, PaymentParameter.BPA_NOTICE));
+        if(chartType.equals("relative")){
+            switch (radio){
+                case "Сумма платежа":
+                {
+                    model.addAttribute("diagram",
+                            ChartGenerator.chartGenerateJSON(PrepareMapToChart.getChartMap(paymentRegistry, PaymentParameter.PAYMENT_AMOUNT), PaymentParameter.PAYMENT_AMOUNT.getName()));
+                    System.out.println("Так выглядит вывод: " + ChartGenerator.chartGenerateJSON(PrepareMapToChart.getChartMap(paymentRegistry, PaymentParameter.PAYMENT_AMOUNT), PaymentParameter.PAYMENT_AMOUNT.getName()));
+                }
+                default:
+                    System.out.println("Неверный тип данных для диаграммы");
+            }
+        }
 
-        System.out.println(prepareData.getPaymentAmount(paymentRegistry, PaymentType.FINES.getName()));
+
+        model.addAttribute("sumPayment", PrepareData.getTotalPaymentAmount(paymentRegistry));
+        System.out.println(PrepareData.getTotalPaymentAmount(paymentRegistry));
+        model.addAttribute("sumBPA", PrepareData.getTotalBPA_notice(paymentRegistry));
+        model.addAttribute("sumPNKO", PrepareData.getTotalPNKO_notice(paymentRegistry));
+        model.addAttribute("sumCash", PrepareData.getTotalCashAmount(paymentRegistry));
+        model.addAttribute("sumCard", PrepareData.getTotalCardAmount(paymentRegistry));
+        model.addAttribute("sumSBP", PrepareData.getTotalSBP_amount(paymentRegistry));
+        model.addAttribute("sumOrgBPA", PrepareData.getTotalOrganization_BPA_notice(paymentRegistry));
+        model.addAttribute("sumOrgPNKO", PrepareData.getTotalOrganization_PNKO_notice(paymentRegistry));
+
+        System.out.println("Chart Type: " + chartType);
+        System.out.println("Selected Radio: " + radio);
 
         return "generationSettings/frame";
     }
+
+
+    @PostMapping("/submitDiagram")
+    public String submitForm(@RequestParam("chartType") String chartType,
+                             @RequestParam("radio") String radio) {
+        System.out.println("Chart Type: " + chartType);
+        System.out.println("Selected Radio: " + radio);
+
+        // Дополнительная логика обработки данных формы может быть добавлена здесь
+
+        return "generationSettings/frame"; // Перенаправление на страницу успешного завершения
+    }
+
 }
