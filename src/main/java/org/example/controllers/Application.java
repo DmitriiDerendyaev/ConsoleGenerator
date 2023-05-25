9e4c143e537c26bbd3ccad5e6348ae41f6c15a41
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-05-26 02:37:48 +0300

    config: конфигурация бинов @EntityScan, @Configuration, @EnableJpaRepository, @ComponentScan

--- src/main/java/org/example/controllers/Application.java
+++ src/main/java/org/example/controllers/Application.java
@@ -2,8 +2,10 @@ package org.example.controllers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("org.example.models")
 public class Application {
 
     public static void main(String[] args) {
