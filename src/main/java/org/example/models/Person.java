5d88b8cba0f1673450ab7fdec0f2ee3809f7be8d
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-05-27 00:33:14 +0300

    feature: добавить логин и пароль, обновить конструктор

--- src/main/java/org/example/models/Person.java
+++ src/main/java/org/example/models/Person.java
@@ -1,9 +1,6 @@
 package org.example.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.util.List;
 
@@ -12,6 +9,7 @@ import java.util.List;
 public class Person {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @OneToMany(mappedBy = "person")
@@ -22,11 +20,38 @@ public class Person {
     private String phone;
     private String email;
     private String surname;
+    private String login;
+    private String password;
+
+    public Person(String name, String patronymic, String phone, String email, String surname, String login, String password) {
+        this.name = name;
+        this.patronymic = patronymic;
+        this.phone = phone;
+        this.email = email;
+        this.surname = surname;
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public Person() {
     }
 
-    // Добавьте геттеры и сеттеры для всех полей
 
     public Long getId() {
         return id;
