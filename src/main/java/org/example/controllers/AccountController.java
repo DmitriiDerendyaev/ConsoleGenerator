439daf7fa248a0e67b01db67e415d0232cb77d28
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-05-27 04:19:08 +0300

    fix: исправить отображение данных в ЛК

--- src/main/java/org/example/controllers/AccountController.java
+++ src/main/java/org/example/controllers/AccountController.java
@@ -11,7 +11,12 @@ public class AccountController {
     @GetMapping("/account")
     public String account(Model model, HttpServletRequest request){
         HttpSession session = request.getSession();
+        model.addAttribute("variableName", session.getAttribute("variableName"));
         model.addAttribute("variableSurname", session.getAttribute("variableSurname"));
+        model.addAttribute("variableRole", session.getAttribute("variableRole"));
+        model.addAttribute("variablePatronymic", session.getAttribute("variablePatronymic"));
+        model.addAttribute("variableEmail", session.getAttribute("variableEmail"));
+        model.addAttribute("variablePhoneNumber", session.getAttribute("variablePhoneNumber"));
         return "personalAccount/frame";
     }
 
